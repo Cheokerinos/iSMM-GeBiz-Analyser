7f4c0e17e67049e931cc39175649df3e27a20d11
Cheokerinos <cheokbryan@gmail.com>
2025-05-26 17:31:21 +0800

    updated scraper

--- Tender-Scraper/scraper.py
+++ Tender-Scraper/scraper.py
@@ -11,58 +11,13 @@ import pandas as pd
 
 def init_driver(): #this function helps to initialize the Chrome driver
     options = Options()
-    # options.add_argument('--headless')
+    options.add_argument('--headless')
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-dev-shm-usage')
     
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
     return driver
 
-def scrape_tender():#main function to scrape the tender listings.
-    driver = init_driver()
-    url = 'https://www.gebiz.gov.sg/' #GeBiz URL
-    driver.get(url)
-    wait = WebDriverWait(driver, 10) #wait for the page to load
-    try:
-        print("Finding the search box…")
-        
-        #wait for the searh box to load
-        kw = wait.until(EC.presence_of_element_located((
-        By.ID, "contentForm:j_idt179_searchBar_INPUT-SEARCH"
-        )))
-    except Exception as e:
-        print("Failed to load the search box.")
-        return
-    kw.clear()                                                                                  #clear the search box
-    print("Entering search term…")
-    old = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.CLASS_NAME, "commandLink_TITLE-BLUE"))
-    )                                                                                           #Find the old element to wait for staleness
-    
-    kw.send_keys("Facilities Management")                                                       #entering the keywords for search
-    
-    go_btn = wait.until(
-        EC.element_to_be_clickable((By.ID, "contentForm:j_idt179_searchBar_BUTTON-GO")))        #wait for the search button to be clickable
-    
-    go_btn.click()
-    print("Clicking search button…")
-    WebDriverWait(driver, 15).until(EC.staleness_of(old))                                       #wait for the old element to be stale
-
-    try:
-        #wait for new search results to load
-        wait.until(
-            EC.presence_of_element_located((By.CLASS_NAME, "commandLink_TITLE-BLUE"))
-        )
-        
-        #scrape the tender titles
-        titles = driver.find_elements(By.CLASS_NAME, "commandLink_TITLE-BLUE")
-        
-        for i, title in enumerate(titles, 1):
-            print(f"{i}. {title.text.strip()}")
-    except Exception as e:
-        print("Failed to find tender listings.")
-        print(e)
-    driver.quit()
     
 def scrape_by_keyword(keyword):
     results = []
